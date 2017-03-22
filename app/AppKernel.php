06a97e55c0086d337771a5eab95a18ded8163454
Yurii K <yurii-github@users.noreply.github.com>
2017-03-22 19:40:00 +0200

    checking jms bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Yurii\OroBundle\OroBundle(),
+          
+          // checking
+          new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+          // not required, but recommended for better extraction
+         // new JMS\TranslationBundle\JMSTranslationBundle(),
+          
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
