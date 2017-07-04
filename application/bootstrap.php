c01fd2ac85ad8d2ce7d66cf2744ce8031d91ac80
Yurii K <yurii-github@users.noreply.github.com>
2017-07-04 16:13:07 +0300

    playing with routes

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -145,12 +145,8 @@ Kohana::modules(array(
  */
 // Cookie::$salt = NULL;
 
+
 /**
- * Set the routes. Each route must have a minimum of a name, a URI and a set of
- * defaults for the URI.
+ * Include separate routes file
  */
-Route::set('default', '(<controller>(/<action>(/<id>)))')
-	->defaults(array(
-		'controller' => 'welcome',
-		'action'     => 'index',
-	));
+require_once APPPATH.'config/routes.php';
