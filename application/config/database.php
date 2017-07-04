c90bc10fce0cf514df7d14e5422f2b1f82e5c84f
Yurii K <yurii-github@users.noreply.github.com>
2017-07-04 20:46:48 +0300

    use only PDO

--- application/config/database.php
+++ application/config/database.php
@@ -5,120 +5,34 @@ if (file_exists(__DIR__.'/database.local.php')) {
 }
 
 
+<?php
 return array
 (
-	'default' => array
-	(
-		'type'       => 'MySQL',
-		'connection' => array(
-			/**
-			 * The following options are available for MySQL:
-			 *
-			 * string   hostname     server hostname, or socket
-			 * string   database     database name
-			 * string   username     database username
-			 * string   password     database password
-			 * boolean  persistent   use persistent connections?
-			 * array    variables    system variables as "key => value" pairs
-			 *
-			 * Ports and sockets may be appended to the hostname.
-			 */
-			'hostname'   => 'localhost',
-			'database'   => 'tstech_test',
-			'username'   => FALSE,
-			'password'   => FALSE,
-			'persistent' => FALSE,
-		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-	),
-	'alternate_' => array
-	(
-		'type'       => 'MySQLi',
-		'connection' => array(
-			/**
-			 * The following options are available for MySQLi:
-			 *
-			 * string   hostname     server hostname, or socket
-			 * string   database     database name
-			 * string   username     database username
-			 * string   password     database password
-			 * boolean  persistent   use persistent connections?
-			 * array    ssl          ssl parameters as "key => value" pairs.
-			 *                       Available keys: client_key_path, client_cert_path, ca_cert_path, ca_dir_path, cipher
-			 * array    variables    system variables as "key => value" pairs
-			 *
-			 * Ports and sockets may be appended to the hostname.
-			 */
-			'hostname'   => 'localhost',
-			'database'   => 'kohana',
-			'username'   => FALSE,
-			'password'   => FALSE,
-			'persistent' => FALSE,
-			'ssl'        => NULL,
-		),
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-	),
-	'alternate' => array(
-		'type'       => 'PDO',
-		'connection' => array(
-			/**
-			 * The following options are available for PDO:
-			 *
-			 * string   dsn         Data Source Name
-			 * string   username    database username
-			 * string   password    database password
-			 * boolean  persistent  use persistent connections?
-			 */
-			'dsn'        => 'mysql:host=localhost;dbname=kohana',
-			'username'   => 'root',
-			'password'   => 'r00tdb',
-			'persistent' => FALSE,
-		),
-		/**
-		 * The following extra options are available for PDO:
-		 *
-		 * string   identifier  set the escaping identifier
-		 */
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => FALSE,
-	),
-	/**
-	 * MySQLi driver config information
-	 *
-	 * The following options are available for MySQLi:
-	 *
-	 * string   hostname     server hostname, or socket
-	 * string   database     database name
-	 * string   username     database username
-	 * string   password     database password
-	 * boolean  persistent   use persistent connections?
-	 * array    ssl          ssl parameters as "key => value" pairs.
-	 *                       Available keys: client_key_path, client_cert_path, ca_cert_path, ca_dir_path, cipher
-	 * array    variables    system variables as "key => value" pairs
-	 *
-	 * Ports and sockets may be appended to the hostname.
-	 *
-	 * MySQLi driver config example:
-	 *
-	 */
-// 	'alternate_mysqli' => array
-// 	(
-// 		'type'       => 'MySQLi',
-// 		'connection' => array(
-// 			'hostname'   => 'localhost',
-// 			'database'   => 'kohana',
-// 			'username'   => FALSE,
-// 			'password'   => FALSE,
-// 			'persistent' => FALSE,
-// 			'ssl'        => NULL,
-// 		),
-// 		'table_prefix' => '',
-// 		'charset'      => 'utf8',
-// 		'caching'      => FALSE,
-// 	),
+    'default' => array
+    (
+        'type'       => 'PDO',
+        'connection' => array(
+            /**
+             * The following options are available for PDO:
+             *
+             * string   dsn         Data Source Name
+             * string   username    database username
+             * string   password    database password
+             * boolean  persistent  use persistent connections?
+             */
+            'dsn'        => 'mysql:host=localhost;dbname=tstech_test',
+            'username'   => 'root',
+            'password'   => 'YOURPASS',
+            'persistent' => FALSE,
+        ),
+        /**
+         * The following extra options are available for PDO:
+         *
+         * string   identifier  set the escaping identifier
+         */
+        'table_prefix' => '',
+        'charset'      => 'utf8',
+        'caching'      => FALSE,
+    ),
 );
+
