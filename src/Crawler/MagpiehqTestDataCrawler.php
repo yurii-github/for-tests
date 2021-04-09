80ed437de88532ad89bdc29a450f03c84376c244
Yurii K <yurii-github@users.noreply.github.com>
2021-04-09 23:02:33 +0300

    refactor

--- src/Crawler/MagpiehqTestDataCrawler.php
+++ src/Crawler/MagpiehqTestDataCrawler.php
@@ -1,16 +1,15 @@
 <?php
 
-namespace tests;
+namespace App\Crawler;
 
-use App\Crawler\MagpiehqCrawler;
 use Symfony\Component\DomCrawler\Crawler;
 
-class MagpiehqTestCrawler extends MagpiehqCrawler
+class MagpiehqTestDataCrawler extends MagpiehqCrawler
 {
     protected function fetchPage(int $page): Crawler
     {
         $pageUrl = $this->buildPagedUrl($page);
-        $testDataFile = __DIR__."/data/magpiehq/page{$page}.html";
+        $testDataFile = dirname(__DIR__,2)."/tests/data/magpiehq/page{$page}.html";
         $html = file_exists($testDataFile) ? file_get_contents($testDataFile) : '';
         return new Crawler($html, $pageUrl);
     }
--- tests/Unit/MagpiehqCrawlerTest.php
+++ tests/Unit/MagpiehqCrawlerTest.php
@@ -6,7 +6,7 @@ use App\Crawler\CrawlerInterface;
 use App\Crawler\MagpiehqCrawler;
 use App\ProductCollection;
 use PHPUnit\Framework\TestCase;
-use tests\MagpiehqTestCrawler;
+use App\Crawler\MagpiehqTestDataCrawler;
 
 class MagpiehqCrawlerTest extends TestCase
 {
@@ -20,7 +20,7 @@ class MagpiehqCrawlerTest extends TestCase
         $this->assertInstanceOf(ProductCollection::class, $products);
         $this->assertCount(21, $products);
 
-        if ($crawler instanceof MagpiehqTestCrawler) {
+        if ($crawler instanceof MagpiehqTestDataCrawler) {
             $this->assertSame(file_get_contents(dirname(__DIR__) . '/data/magpiehq/all_products.json'),
                 json_encode($products, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
             );
@@ -31,7 +31,7 @@ class MagpiehqCrawlerTest extends TestCase
     public function providesCrawlers()
     {
         return [
-            [new MagpiehqTestCrawler()],
+            [new MagpiehqTestDataCrawler()],
            // [new MagpiehqCrawler()]
         ];
     }
